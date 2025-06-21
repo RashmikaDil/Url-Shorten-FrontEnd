f4b09aa2f283717ee6f18730e72a55942da8cc27
Rashmika Dillhara <rashmikadil2023@gmail.com>
2025-06-21 13:10:30 +0530

    Completed Project

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,8 @@ const setSceenComponent = (screen) => {
 <div className='md:h-screen h-auto bg-gray-900 text-white   pb-20 flex-col flex items-center justify-center'>  <img src={logo} alt="Logo" className='w-50 h-50 ' />
     <div className='w-[90% ] mb-0   md:w-[50%] lg:w-[30%] overflow-hidden bg-gray-800 flex justify-between  items-center  rounded-lg shadow-lg mt-0'>
   
-  <div onClick={() => setSceenComponent('Short')} className={`w-1/2 h-full p-4 flex justify-center text-white items-center cursor-pointer  ${screen === 'Short' ? 'bg-blue-500': 'bg-gray-800'}`}>URL Shortener</div>
-  <div onClick={() => setSceenComponent('Track')} className={`w-1/2 h-full p-4 flex justify-center text-white items-center cursor-pointer ${screen === 'Track' ? 'bg-blue-500': 'bg-gray-800'}`}>Track URL</div>
+  <div onClick={() => setSceenComponent('Short')} className={`w-1/2 h-full p-4 flex justify-center text-white items-center cursor-pointer  ${screen === 'Short' ? 'bg-blue-500': 'bg-gray-800'}`}>Short</div>
+  <div onClick={() => setSceenComponent('Track')} className={`w-1/2 h-full p-4 flex justify-center text-white items-center cursor-pointer ${screen === 'Track' ? 'bg-blue-500': 'bg-gray-800'}`}>Track</div>
 </div>
   <div className=' m-0 p-0 flex mt-10 flex-col  items-center bg-gray-900'>
       
